701267c03aee2d90dc47122606e3b69d3920b010
Luca Colonnello <luca.colonnello91@gmail.com>
2015-08-06 20:19:19 +0200

    First commit

---
+++ server/boot/explorer.js
@@ -0,0 +1,12 @@
+var explorer = require('loopback-explorer');
+
+module.exports = function(server) {
+  var restApiRoot = server.get('restApiRoot');
+
+  var explorerApp = explorer(server, { basePath: restApiRoot });
+  server.use('/explorer', explorerApp);
+  server.once('started', function() {
+    var baseUrl = server.get('url').replace(/\/$/, '');
+    console.log('Browse your REST API at %s%s', baseUrl, explorerApp.mountpath);
+  });
+};
